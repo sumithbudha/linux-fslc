771bae00bb05e90992d2aaf980115452a05abca9
sumithbudha <51715301+sumithbudha@users.noreply.github.com>
2019-09-16 15:15:12 +0530

    Update super.c

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -5443,12 +5443,15 @@ static int ext4_enable_quotas(struct super_block *sb)
 	for (type = 0; type < EXT4_MAXQUOTAS; type++) {
 		if (qf_inums[type]) {
 			err = ext4_quota_enable(sb, type, QFMT_VFS_V1,
-						DQUOT_USAGE_ENABLED);
+				DQUOT_USAGE_ENABLED |
+				(quota_mopt[type] ? DQUOT_LIMITS_ENABLED : 0));
 			if (err) {
 				ext4_warning(sb,
 					"Failed to enable quota tracking "
 					"(type=%d, err=%d). Please run "
 					"e2fsck to fix.", type, err);
+				for (type--; type >= 0; type--)
+					dquot_quota_off(sb, type);
 				return err;
 			}
 		}
